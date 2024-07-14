ef9dfb7f015325b51e44bce85d1d175af0d79f8b
MaPitelli <mairapitelli@hotmail.com>
2024-07-14 11:41:55 +0200

    9th exercise concluded

--- evaluacion_maira_pitelli.sql
+++ evaluacion_maira_pitelli.sql
@@ -41,3 +41,8 @@ SELECT title -- , rating (si quisiéramos visualizar también la clasificación)
 FROM film
 WHERE rating NOT IN ('R', 'PG-13');
 
+-- 9. Encuentra la cantidad total de películas en cada clasificación de la tabla film y muestra la clasificación junto con el recuento.
+SELECT COUNT(film_id) AS total_quantity, rating
+FROM film
+GROUP BY rating;
+
