0d9b9528239320fdc403ebe873ece43d69a51f36
MaPitelli <mairapitelli@hotmail.com>
2024-07-14 12:37:38 +0200

    14th exercise concluded

--- evaluacion_maira_pitelli.sql
+++ evaluacion_maira_pitelli.sql
@@ -73,3 +73,9 @@ INNER JOIN film_actor AS fa USING(actor_id)
 INNER JOIN film AS f USING(film_id)
 WHERE f.title = "Indian Love";
 
+-- 14. Muestra el título de todas las películas que contengan la palabra "dog" o "cat" en su descripción.
+SELECT title, description 
+FROM film
+WHERE description LIKE '%dog%' 
+   OR description LIKE '%cat%';
+
