4d51a45af894947d2e1a82ae44594c80123d92d9
MaPitelli <mairapitelli@hotmail.com>
2024-07-14 23:43:25 +0200

    23rd and 24th exercises concluded

--- evaluacion_maira_pitelli.sql
+++ evaluacion_maira_pitelli.sql
@@ -136,3 +136,20 @@ WHERE r.rental_id IN (SELECT rental_id
  					  FROM rental AS r 
 					  WHERE DAY(return_date) - DAY(rental_date) > 5);
 
+-- 23. Encuentra el nombre y apellido de los actores que no han actuado en ninguna película de la categoría "Horror". Utiliza una subconsulta para encontrar los actores que han actuado en películas de la categoría "Horror" y luego exclúyelos de la lista de actores.
+SELECT ac.first_name, ac.last_name
+FROM actor AS ac
+WHERE actor_id NOT IN (SELECT DISTINCT a.actor_id 
+					   FROM actor AS a 
+					   INNER JOIN film_actor AS fa ON a.actor_id = fa.actor_id
+					   INNER JOIN film_category AS fc ON fa.film_id = fc.film_id
+					   INNER JOIN category AS c ON fc.category_id = c.category_id
+					   WHERE c.name = 'Horror');
+					 
+-- 24. BONUS: Encuentra el título de las películas que son comedias y tienen una duración mayor a 180 minutos en la tabla film.
+SELECT f.title -- , c.name, f.`length`
+FROM film AS f
+INNER JOIN film_category AS fc ON f.film_id = fc.film_id 
+INNER JOIN category AS c ON fc.category_id = c.category_id 
+WHERE c.name = 'Comedy' AND f.`length` > 180;
+
