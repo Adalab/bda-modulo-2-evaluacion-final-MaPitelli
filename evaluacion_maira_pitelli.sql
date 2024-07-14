054b7821c68a9fbe5a410e12e1738ad242cdfd41
MaPitelli <mairapitelli@hotmail.com>
2024-07-14 12:10:21 +0200

    13th exercise concluded

--- evaluacion_maira_pitelli.sql
+++ evaluacion_maira_pitelli.sql
@@ -66,3 +66,10 @@ SELECT round(AVG(`length`), 2) AS Average_duration, rating
 FROM film
 GROUP BY rating;
 
+-- 13. Encuentra el nombre y apellido de los actores que aparecen en la película con title "Indian Love".
+SELECT a.first_name AS name, a.last_name AS surname
+FROM actor AS a 
+INNER JOIN film_actor AS fa USING(actor_id)
+INNER JOIN film AS f USING(film_id)
+WHERE f.title = "Indian Love";
+
