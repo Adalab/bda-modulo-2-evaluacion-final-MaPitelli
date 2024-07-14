b85f3b7bae5c3aa2eca99074207583e1f3a867fb
MaPitelli <mairapitelli@hotmail.com>
2024-07-14 10:22:14 +0200

    Third exercise concluded

--- evaluacion_maira_pitelli.sql
+++ evaluacion_maira_pitelli.sql
@@ -10,3 +10,9 @@ FROM film;
 SELECT title AS titles -- , rating (si quisiéramos visualizar también la clasificación)
 FROM film
 WHERE rating = 'PG-13';
+
+-- 3. Encuentra el título y la descripción de todas las películas que contengan la palabra "amazing" en su descripción.
+SELECT title, description
+FROM film
+WHERE description LIKE '%amazing%';
+
