3762066e977f16ad9067f5f8f1fc15507a16ff2e
MaPitelli <mairapitelli@hotmail.com>
2024-07-14 10:18:20 +0200

    Second exercise concluded

---
+++ evaluacion_maira_pitelli.sql
@@ -0,0 +1,12 @@
+-- Evaluación Final Módulo 2
+
+USE sakila;
+
+-- 1. Selecciona todos los nombres de las películas sin que aparezcan duplicados.
+SELECT DISTINCT title AS titles
+FROM film; 
+
+-- 2. Muestra los nombres de todas las películas que tengan una clasificación de "PG-13".
+SELECT title AS titles -- , rating (si quisiéramos visualizar también la clasificación)
+FROM film
+WHERE rating = 'PG-13';
