d05cfbf4af09b9f9b088890b66f35889a94f2fff
MaPitelli <mairapitelli@hotmail.com>
2024-07-14 13:08:23 +0200

    15th exercise concluded

--- evaluacion_maira_pitelli.sql
+++ evaluacion_maira_pitelli.sql
@@ -79,3 +79,8 @@ FROM film
 WHERE description LIKE '%dog%' 
    OR description LIKE '%cat%';
 
+-- 15. Hay algún actor o actriz que no apareca en ninguna película en la tabla film_actor.
+SELECT actor_id
+FROM actor
+WHERE actor_id NOT IN (SELECT DISTINCT actor_id FROM film_actor);
+
